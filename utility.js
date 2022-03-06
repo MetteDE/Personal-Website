407ac6f782b116ec071e2500b780d5363046a5d3
Mathis <mattyyytrd@gmail.com>
2022-03-06 13:46:59 +0100

    new style

--- utility.js
+++ utility.js
@@ -1,3 +1,5 @@
+
+// mute toggle button //
 function toggleMute() {
   var video = document.getElementById("myVideo");
   if(video.muted){
@@ -9,21 +11,29 @@ function toggleMute() {
     document.getElementById("toggle").innerHTML = "unmute"
 }
 }
-
+// during the video //
 function videoStarts(){
+  // set default audio volume //
   var video = document.getElementById("myVideo");
   video.volume = 0.2;
+  // hide contextmenu during the video //
   document.oncontextmenu = function() {
     return false;
   }
 }
 
-// Hide video after it ends // 
+// after the video // 
 function videoEnds() {
+  // hide video after it ended //
   var video = document.getElementById("myVideo");
   video.style.display = "None" 
+  // show contextmenu after the video ended //
   document.oncontextmenu = function() {
     return true;
   }
-} 
 
+
+  // testing //
+  //document.body.style.backgroundColor = "white";
+} 
+//
